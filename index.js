c048418794d2908933abfa8137bf53ab73334cfd
Pat Lenehan <patrick.lenehan@libertymutual.com>
2020-12-04 22:17:02 -0500

    New official dumb dumb

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Lukas a dumb dumb'),
+    body: JSON.stringify('I am the dumb dumb'),
   };
   return response;
 };
